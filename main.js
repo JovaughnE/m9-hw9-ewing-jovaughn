482aaa476baef64ae405525271227b15ecddca3d
Jovaughn E <jovaughn.ewing@ufl.edu>
2021-03-11 22:32:42 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+class Desserts {
+    constructor(type, calories) {
+        this.type
+        this.calories
+    }
+    iceCream () {
+        this.inheritFrom = Desserts;
+        constructor(flavor, scoops)
+        super(flavor, scoops)
+        this.flavor
+        this.scoops
+        includeSpoon() {
+            console.log("Here is your spoon")
+        }
+    }
+}
+
